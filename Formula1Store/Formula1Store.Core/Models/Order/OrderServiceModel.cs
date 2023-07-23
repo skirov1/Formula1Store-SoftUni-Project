a64999e1916aad4434581933074464200bae22e2
Simeon Kirov <simeonkirov21@gmail.com>
2023-07-23 14:01:45 +0300

    Added view for all orders

---
+++ Formula1Store/Formula1Store.Core/Models/Order/OrderServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Formula1Store.Core.Models.Order
+{
+    public class OrderServiceModel
+    {
+        public int OrderId { get; set; }
+
+        public string OrderDate { get; set; } = null!;
+    }
+}
---
+++ Formula1Store/Formula1Store.Core/Models/Order/OrdersViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Formula1Store.Core.Models.Order
+{
+    public class OrdersViewModel
+    {
+        public List<OrderServiceModel> Orders { get; set; } = new List<OrderServiceModel>();
+    }
+}
